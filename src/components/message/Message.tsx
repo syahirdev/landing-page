bcd954965c0ef1cdf8223da18d65257ff6bd13c1
Syahir <67471898+Syahirdev@users.noreply.github.com>
2021-12-01 22:15:45 +0800

    add message form component

--- src/components/message/Message.tsx
+++ src/components/message/Message.tsx
@@ -3,7 +3,23 @@ import "./Message.scss";
 export const Message = () => {
     return (
         <div className="message">
-            message
+            <div className="messageTitle">Register Your Interest</div>
+            <div className="formMessage">
+                <div className="formInput">
+                    <input type="text" placeholder="Name"/>
+                    <input type="text" placeholder="Occupation"/>
+                </div>
+                <div className="formInput">
+                    <input type="text" placeholder="Email Address"/>
+                    <input type="text" placeholder="Contact No"/>
+                </div>
+                <div className="formInput">
+                    <textarea placeholder="Your Message..."/>
+                </div>
+                <button type="submit" className="submitButton">
+                    SUBMIT MESSAGE
+                </button>
+            </div>
         </div>
     );
 };
