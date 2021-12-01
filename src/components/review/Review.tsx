915a4942e788ae3bc59c362065a8055a93e89a44
Syahir <67471898+Syahirdev@users.noreply.github.com>
2021-12-01 22:46:07 +0800

    add review component

--- src/components/review/Review.tsx
+++ src/components/review/Review.tsx
@@ -1,9 +1,28 @@
 import "./Review.scss";
+import { ArrowLeft, ArrowRight, FormatQuote } from "@mui/icons-material";
 
 export const Review = () => {
     return (
         <div className="review">
-            review
+            <div className="reviewCard">
+                <span className="reviewerName">ALEXIS SIMPSON</span>
+                <span className="reviewerTitle"> - CEO & Developer</span>
+            </div>
+
+            <div className="reviewQuotes">
+                <FormatQuote className='invertedQuote'/>
+                Lorem ipsum dolor sit amet, laudantium, totam rem.
+                Morbi sagittis, sem quis lacinia faucibus,
+                orci ipsum gravida tortor,
+                vel interdum mi sapien ut justo consequat magna,
+                id molestie ipsum volutpat quis.
+                <FormatQuote/>
+            </div>
+
+            <div className="reviewArrow">
+                <ArrowLeft className="leftArrow"/>
+                <ArrowRight className="rightArrow"/>
+            </div>
         </div>
     );
 };
