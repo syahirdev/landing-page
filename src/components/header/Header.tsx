0bc3d8c9d1a72d1f23015550643a81279bfd5ef0
Syahir <67471898+Syahirdev@users.noreply.github.com>
2021-12-01 14:43:41 +0800

    add header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,18 @@
 import "./Header.scss";
+import { KeyboardArrowDown } from "@mui/icons-material";
 
 export const Header = () => {
     return (
-        <div className={"header"}>
-            test
+        <div className="header">
+            <div className="wrapper">
+                <div className="smallText">WHAT ARE YOU WAITING FOR?</div>
+                <div className="bigText">LET'S BE CREATIVE !</div>
+                <div className="containerText">LOREM IPSUM DOLOR SI AMET</div>
+                <div className="mottoText">
+                    <span className="firstLetter">START</span> DOING THAT
+                </div>
+                <KeyboardArrowDown className="icon"/>
+            </div>
         </div>
     );
 };
